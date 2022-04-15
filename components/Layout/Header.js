d553e808306a3b531ba8d0f11d2c852159cee85b
Wei Lun <garyganweilun@gmail.com>
2022-04-16 03:51:19 +0800

    Welcome page done

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,7 +1,44 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, useToast } from '@chakra-ui/react'
+import axios from 'axios'
 import Link from 'next/link'
+import { useRouter } from "next/router";
 
 const Header = (children) => {
+  const toast = useToast();
+  const router = useRouter();
+  const handleLogOut = async () => {
+    await axios.delete(`${process.env.NEXT_PUBLIC_backendURL}/logout`,
+      { withCredetnials: true }).then((res) => {
+        if (res.status === 200) {
+          toast({
+            title: "Successfully logged Out",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          })
+          router.push('/');
+
+        }
+        else {
+          toast({
+            title: "Error logging Out",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      }).catch((err) => {
+        if (err) {
+
+          toast({
+            title: "Error logging Out",
+            status: "error",
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      })
+  }
 
   return (
     <>
@@ -15,7 +52,9 @@ const Header = (children) => {
           WookieWars
         </Box>
         <Box bg='black' w='100%' textAlign={"right"} color='Gold' border='1px' borderLeft="0px" borderTop="0px">
-          Logout
+          <Button variant='link' color="gold" onClick={handleLogOut}>
+            Logout
+          </Button>
         </Box>
       </Flex> : ""}
     </>
---
+++ components/WelcomePage/CategoryCards.js
@@ -0,0 +1,58 @@
+import { Box, Button, Center, SimpleGrid } from "@chakra-ui/react"
+import Link from "next/link"
+
+
+const CategoryCards = () => {
+    return (
+        <Center>
+            <Box w="80vh" >
+                <SimpleGrid columns={2} spacing={10} textAlign="center">
+                    <Center>
+                        <Link href="/flims">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                Flims
+                            </Button>
+                        </Link>
+                    </Center>
+                    <Center>
+                        <Link href="/people">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                People
+                            </Button>
+                        </Link>
+                    </Center>
+                    <Center>
+                        <Link href="/planets">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                Planets
+                            </Button>
+                        </Link>
+                    </Center>
+                    <Center>
+                        <Link href="/species">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                Species
+                            </Button>
+                        </Link>
+                    </Center>
+                    <Center>
+                        <Link href="/starships">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                StarShips
+                            </Button>
+                        </Link>
+                    </Center>
+                    <Center>
+                        <Link href="/vehicles">
+                            <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow">
+                                Vehicles
+                            </Button>
+                        </Link>
+                    </Center>
+                </SimpleGrid>
+            </Box>
+
+        </Center>
+    )
+}
+export default CategoryCards
