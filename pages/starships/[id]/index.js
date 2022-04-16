17afa72ec7d92e787033ec97fb96c99005c0969c
Wei Lun <garyganweilun@gmail.com>
2022-04-16 16:23:52 +0800

    Starship-Feature-done

--- pages/starships/[id]/index.js
+++ pages/starships/[id]/index.js
@@ -1,18 +1,37 @@
+import axios from "axios"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import Body from "../../../components/Layout/Body"
 import Header from "../../../components/Layout/Header"
+import BackButton from "../../../components/BackButton"
+import StarShipsDetails from "../../../components/StarShipsPage/StarShipDetails"
 
-const Starship = () => {
+const StarShip = () => {
+    const [getStarShipDetails, setGetStarShipDetails] = useState([]);
     const router = useRouter()
     const { id } = router.query
+
+    useEffect(() => {
+        const getStarShip = () => {
+            axios.get(`${process.env.NEXT_PUBLIC_SWAPI_URL}/starships/?search=${id}`)
+                .then((res) => {
+                    setGetStarShipDetails(res.data.results)
+                })
+        }
+        getStarShip()
+    }, [id]);
+
+
     return (
         <>
             <Header Loggedin={true} />
             <Body>
-                This is Starship {id}
+                <StarShipsDetails StarShipDetails={getStarShipDetails} />
+                <BackButton backTo={"starships"} />
             </Body>
+
         </>
     )
 }
 
-export default Starship
+export default StarShip
