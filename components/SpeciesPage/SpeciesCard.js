7d4d2308782c68e9474a3ff29339472a258aff42
Wei Lun <garyganweilun@gmail.com>
2022-04-16 15:38:20 +0800

    Species-Feature done

---
+++ components/SpeciesPage/SpeciesCard.js
@@ -0,0 +1,76 @@
+import { Box, Button, Center, SimpleGrid } from "@chakra-ui/react";
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from "axios";
+import { useEffect, useState } from "react"
+import ReactPaginate from "react-paginate";
+import Link from "next/link"
+
+
+
+const SpeciesCard = () => {
+    const [getSpecies, setGetSpecies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const PER_PAGE = 4;
+    const offset = currentPage * PER_PAGE;
+    const pageCount = Math.ceil(getSpecies.length / PER_PAGE);
+
+    useEffect(() => {
+        const species = () => {
+            axios.get(`${process.env.NEXT_PUBLIC_SWAPI_URL}/species/`)
+                .then((res) => {
+                    setGetSpecies(res.data.results)
+                })
+        }
+        species()
+    }, []);
+
+    const handlePageClick = ({ selected: selectedPage }) => {
+        setCurrentPage(selectedPage)
+    }
+
+    const getSpeciesResult = getSpecies.slice(offset, offset + PER_PAGE).map((data, index) => {
+        return <Center key={index}>
+            <Link href={`/species/${data.name}`}>
+                <Button w='80%' h="100px" borderWidth='1px' borderRadius='lg' colorScheme="yellow" >
+                    {data.name}
+                </Button>
+            </Link>
+        </Center>
+    })
+
+
+
+    return (
+        <>
+            <Center>
+                <Box w="80vh" >
+                    <SimpleGrid columns={2} spacing={10} textAlign="center">
+                        {getSpeciesResult}
+                    </SimpleGrid>
+                </Box>
+            </Center >
+            <Center mt="20" bg="black">
+                <ReactPaginate
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageCount={pageCount}
+                    previousLabel="< previous"
+                    pageRangeDisplayed={5}
+                    renderOnZeroPageCount={null}
+                    breakClassName={'page-item'}
+                    breakLinkClassName={'page-link'}
+                    containerClassName={'pagination'}
+                    pageClassName={'page-item'}
+                    pageLinkClassName={'page-link'}
+                    previousClassName={'page-item'}
+                    previousLinkClassName={'page-link'}
+                    nextClassName={'page-item'}
+                    nextLinkClassName={'page-link'}
+                    activeClassName={'active'}
+                />
+            </Center>
+        </>
+    )
+}
+
+export default SpeciesCard
--- pages/species/index.js
+++ pages/species/index.js
@@ -1,12 +1,18 @@
 import Body from "../../components/Layout/Body"
 import Header from "../../components/Layout/Header"
-
+import InstructionText from "../../components/InstructionText"
+import BackButton from "../../components/BackButton"
+import SpeciesCard from "../../components/SpeciesPage/SpeciesCard"
 const Species = () => {
     return (
         <>
             <Header Loggedin={true} />
             <Body>
-                This is species
+                <InstructionText>
+                    Select a Species to find out more!
+                </InstructionText>
+                <SpeciesCard />
+                <BackButton backTo={"welcome"} />
             </Body>
         </>
     )
